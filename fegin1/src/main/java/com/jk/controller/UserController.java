6e5ba71bcf3908dbb3d4287c85dc98d605413548
Your Name <3474458278@qq.com>
2021-01-14 11:36:30 +0800

    第四次tijiao

--- fegin1/src/main/java/com/jk/controller/UserController.java
+++ fegin1/src/main/java/com/jk/controller/UserController.java
@@ -4,12 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @program: springcloud-feign
- * @description:
- * @author: 刘洋朋
- * @create: 2021-01-08 19:15
- */
+
 @Controller
 @RequestMapping("user")
 public class UserController {
