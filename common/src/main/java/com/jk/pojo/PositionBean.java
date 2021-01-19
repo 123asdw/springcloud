97576c4995cabbc1380e097cb0ce7ac1e6d36acb
Your Name <3474458278@qq.com>
2021-01-19 15:12:35 +0800

    55

--- common/src/main/java/com/jk/pojo/PositionBean.java
+++ common/src/main/java/com/jk/pojo/PositionBean.java
@@ -4,12 +4,48 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
 public class PositionBean implements Serializable {
 
     private static final long serialVersionUID = 4795724588193493411L;
 
     private Integer positionId;
-    private String positionName;//职位名称
+    private String positionName;
+    private Integer page;
+    private  Integer rows;
 
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Integer positionId) {
+        this.positionId = positionId;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
 }
