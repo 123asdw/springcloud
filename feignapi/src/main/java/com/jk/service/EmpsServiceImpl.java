56905b94fa55922cd61224cf9e6a424d2538dfad
Your Name <3474458278@qq.com>
2021-01-19 15:15:37 +0800

    55

--- feignapi/src/main/java/com/jk/service/EmpsServiceImpl.java
+++ feignapi/src/main/java/com/jk/service/EmpsServiceImpl.java
@@ -5,51 +5,39 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
-
-
 @Component
 public class EmpsServiceImpl implements EmpsService {
-
     @Override
     public String findUser() {
         return "网路异常！！！！";
     }
-
     @Override
     public HashMap<String, Object> findEmp(EmpBean food) {
         return null;
     }
-
     @Override
     public void addEmp(EmpBean food) {
-
     }
-
     @Override
     public EmpBean findEmpByIds(Integer id) {
         return null;
     }
-
     @Override
     public void delEmpByIds(Integer id) {
 
     }
-
     @Override
     public List<DepartmentBean> findDept() {
         return null;
     }
-
     @Override
     public List<PositionBean> findPosition() {
         return null;
     }
-
     @Override
     public List<treeBean> findTree() {
         return null;
     }
-
     @Override
     public userBean findUserByAcoount(String account) {
         return null;
@@ -59,14 +47,44 @@ public class EmpsServiceImpl implements EmpsService {
     public HashMap<String, Object> findList(materialBean bean) {
         return null;
     }
-
     @Override
     public HashMap<String, Object> finduserList(logionBean bean) {
         return null;
     }
-
     @Override
     public HashMap<String, Object> findprizeList(prizeBean prize) {
+        return null;
+    }
+    @Override
+    public HashMap<String, Object> finduserLists(materialBean bean) {
+        return null;
+    }
+    @Override
+    public void add(materialBean bean) {
+    }
+    @Override
+    public HashMap<String, Object> findxiangqingList(PositionBean posit) {
+        return null;
+    }
+    @Override
+    public HashMap<String, Object> find(yongbean yong) {
+        return null;
+    }
+    @Override
+    public void deleteid(Integer id) {
+    }
+    @Override
+    public HashMap<String, Object> findlistdeng(UseridBean user) {
+        return null;
+    }
+    @Override
+    public void addid(prizeBean prize) {
+    }
+    @Override
+    public void deleteid2(Integer id) {
+    }
+    @Override
+    public List<materialBean> findeee() {
         return null;
     }
 }
