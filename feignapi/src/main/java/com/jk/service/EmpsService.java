11f117371c497477d03885b2e6cfe9b9aa187b73
Your Name <3474458278@qq.com>
2021-01-19 15:15:10 +0800

    55

--- feignapi/src/main/java/com/jk/service/EmpsService.java
+++ feignapi/src/main/java/com/jk/service/EmpsService.java
@@ -14,26 +14,18 @@ public interface EmpsService {
 
     @GetMapping("findUser")
     String findUser();
-
     @GetMapping("findEmp")
     HashMap<String, Object> findEmp(@SpringQueryMap EmpBean emp);
-
     @PostMapping("addEmp")
     void addEmp(EmpBean emp);
-
     @GetMapping("findEmpByIds")
     EmpBean findEmpByIds(@RequestParam(value = "id") Integer id);
-
     @DeleteMapping("delEmpByIds")
     void delEmpByIds(@RequestParam(value = "id") Integer id);
-
     @GetMapping("findDept")
     List<DepartmentBean> findDept();
-
     @GetMapping("findPosition")
     List<PositionBean> findPosition();
-
-
    @RequestMapping("findTree")
     List<treeBean> findTree();
     @RequestMapping("findUserByAcoount")
@@ -44,4 +36,22 @@ public interface EmpsService {
     HashMap<String, Object> finduserList(@SpringQueryMap logionBean bean);
     @GetMapping("findprizeList")
     HashMap<String, Object> findprizeList(@SpringQueryMap prizeBean prize);
+    @GetMapping("finduserLists")
+    HashMap<String, Object> finduserLists(@SpringQueryMap materialBean bean);
+    @PostMapping("add")
+    void add(materialBean bean);
+    @GetMapping("findxiangqingList")
+    HashMap<String, Object> findxiangqingList(PositionBean posit);
+    @GetMapping("find")
+    HashMap<String, Object> find(yongbean yong);
+    @DeleteMapping("deleteid")
+    void deleteid(@RequestParam(value = "id")Integer id);
+    @GetMapping("findlistdeng")
+    HashMap<String, Object> findlistdeng(UseridBean user);
+    @PostMapping("addid")
+    void addid(prizeBean prize);
+    @DeleteMapping("deleteid2")
+    void deleteid2(@RequestParam(value = "id")Integer id);
+    @GetMapping("findeee")
+    List<materialBean> findeee();
 }
