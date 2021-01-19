ac2f0ec431831aef616786934f43b036e0b85a01
Your Name <3474458278@qq.com>
2021-01-19 15:17:57 +0800

    55

--- provider/src/main/java/com/jk/service/EmpService.java
+++ provider/src/main/java/com/jk/service/EmpService.java
@@ -9,24 +9,28 @@ import java.util.List;
 public interface EmpService {
     
     HashMap<String, Object> findEmp(EmpBean emp);
-
     void addEmp(EmpBean emp);
-
     EmpBean findEmpByIds(Integer id);
-
     void delEmpByIds(Integer id);
-
     List<DepartmentBean> findDept();
-
     List<PositionBean> findPosition();
-
     List<treeBean> findTree();
-
     userBean findUserByAcoount(String account);
-
     HashMap<String, Object> findList(materialBean bean);
-
     HashMap<String, Object> finduserList(logionBean bean);
-
     HashMap<String, Object> findprizeList(prizeBean prize);
+    HashMap<String, Object> finduserLists(materialBean bean);
+    void add(materialBean bean);
+    HashMap<String, Object> findxiangqingList(PositionBean posit);
+    HashMap<String, Object> find(yongbean yong);
+
+    void deleteid(Integer id);
+
+    HashMap<String, Object> findlistdeng(UseridBean user);
+
+    void addid(prizeBean prize);
+
+    void deleteid2(Integer id);
+
+    List<materialBean> findeee();
 }
